6e2d5d485f360d7723445d2f8c51d75ee166453f
Stevan <stevan14padilha@gmail.com>
2024-03-13 09:47:33 -0300

    feat: page config, read csv

--- index.py
+++ index.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import pandas as pd
 
-st.write("Olá Mundo")
+
+st.set_page_config(layout="wide")
+
+df_reviews = pd.read_csv("./data/customer reviews.csv")
+df_top100_books = pd.read_csv("./data/Top-100 Trending Books.csv")
+
+df_reviews
